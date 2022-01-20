7a05a17f768cb9b5af7efe4cb02ee20ae95547f1
ahkong <ahreum0412@naver.com>
2022-01-20 23:11:47 +0900

    8.유효한 패린드롬

---
+++ src/A20220120_1.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class A20220120_1 {
+
+    public  String solution(String input){
+        String answer = "YES";
+        input = input.toUpperCase().replaceAll("[^A-Z]","");
+        String tmp = new StringBuilder(input).reverse().toString();
+        if(!tmp.equals(input)){
+            answer ="NO";
+        }
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        A20220120_1 a = new A20220120_1();
+        System.out.println(a.solution(input));
+    }
+}
